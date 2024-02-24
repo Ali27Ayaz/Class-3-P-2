1dd7895aac128b3bde6a88caa209969be5655eae
Ali Ayaz <160322443+Ali27Ayaz@users.noreply.github.com>
2024-02-24 14:17:01 +0500

    Add files via upload

---
+++ lesson3.js
@@ -0,0 +1,14 @@
+// Asslam O Alaikum
+console.log();
+var Fname = " My Name is: Ayaz Ali ";
+var Lname = " My Last name is: Shahani ";
+var Cname = " My Course is: ";
+var Cnam = " Type Script , Java Script";
+var PersName = " Hello Sir, Naeem & Ma'am: Syeda Hafsa";
+var coding = 12345;
+console.log(Fname);
+console.log(Lname);
+console.log(Cname + " " + Cnam);
+console.log(PersName + " I am submitting this Exercise to You, plz guide if any mistake");
+console.log();
+console.log("My code is: " + coding);
---
+++ lesson3.ts
@@ -0,0 +1,16 @@
+// Asslam O Alaikum
+console.log(   )
+let Fname: string= " My Name is: Ayaz Ali "
+let Lname: string= " My Last name is: Shahani "
+let Cname: string= " My Course is: "
+let Cnam: string= " Type Script , Java Script"
+let PersName: string= " Hello Sir, Naeem & Ma'am: Syeda Hafsa"
+
+let coding: number= 12345
+console.log (Fname)
+console.log (Lname)
+console.log(Cname + " " + Cnam)
+console.log (PersName + " I am submitting this Exercise to You, plz guide if any mistake")
+console.log(   )
+console.log("My code is: " + coding)
+
